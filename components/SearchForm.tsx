cc60792382bc842ec1c84f00f42cdde57c79d308
MarufHasan-dev <170166811+MarufHasan-dev@users.noreply.github.com>
2025-07-13 21:09:59 +0600

    add search form

---
+++ components/SearchForm.tsx
@@ -0,0 +1,24 @@
+import Form from "next/form";
+import SearchFormReset from "@/components/SearchFormReset";
+import { Search } from "lucide-react";
+
+function SearchForm({ query }: { query?: string }) {
+  return (
+    <Form action="/" className="search-form">
+      <input
+        name="query"
+        defaultValue={query}
+        className="search-input z-30"
+        placeholder="Search Startups"
+      />
+      <div className="flex gap-2">
+        {query && <SearchFormReset />}
+        <button type="submit" className="search-btn text-white">
+          <Search className="size-5" />
+        </button>
+      </div>
+    </Form>
+  );
+}
+
+export default SearchForm;
