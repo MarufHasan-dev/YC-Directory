08bc4965a1b88158d0d091ecc8aa19f1728fa31b
MarufHasan-dev <170166811+MarufHasan-dev@users.noreply.github.com>
2025-09-16 11:09:36 +0600

    feat: implement view count update and add write client

--- components/View.tsx
+++ components/View.tsx
@@ -1,13 +1,20 @@
 import { client } from "@/sanity/lib/client";
 import Ping from "@/components/Ping";
 import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
+import { writeClient } from "@/sanity/lib/write-client";
+import { after } from "next/server";
 
 async function View({ id }: { id: string }) {
   const { views: totalViews } = await client
     .withConfig({ useCdn: false })
     .fetch(STARTUP_VIEWS_QUERY, { id });
 
-  //TODO: Update the number of views
+  after(async () => {
+    await writeClient
+      .patch(id)
+      .set({ views: totalViews + 1 })
+      .commit();
+  });
 
   return (
     <div className="view-container">
