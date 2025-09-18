293e176f92ae0d850bd65a896f08a47dd62c0bec
MarufHasan-dev <170166811+MarufHasan-dev@users.noreply.github.com>
2025-09-18 14:13:10 +0600

    feat: add user profile page with user details and startups; implement UserStartups and StartupCard components

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,9 +1,10 @@
-import { formatDate } from "@/lib/utils";
+import { cn, formatDate } from "@/lib/utils";
 import { EyeIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { Author, Startup } from "@/sanity.types";
+import { Skeleton } from "./ui/skeleton";
 
 export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
 
@@ -42,8 +43,8 @@ export default function StartupCard({ post }: { post: StartupTypeCard }) {
 
         <Link href={`/user/${author?._id}`}>
           <Image
-            src="https://placehold.co/400"
-            alt="placeholder"
+            src={author?.image}
+            alt={author?.name}
             width={48}
             height={48}
             className="rounded-full"
@@ -74,3 +75,13 @@ export default function StartupCard({ post }: { post: StartupTypeCard }) {
     </li>
   );
 }
+
+export const StartupCardSkeleton = () => (
+  <>
+    {[0, 1, 2, 3, 4].map((index: number) => {
+      <li key={cn("skeleton", index)}>
+        <Skeleton className="startup-card-skeleton" />
+      </li>;
+    })}
+  </>
+);
